76b1391db02db894d26038421a5018b8b4fe94fc
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-12-22 20:06:32 +0100

    Restructure the project to have a 'source' directory for the files, and format them using ruff and isort

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-'''
+"""
 hi!
-'''
+"""
 
 import numpy as np
-from CCA import CCA_subcluster
+
+from pyfracval.CCA import CCA_subcluster
 
 # config
 DF = 1.8
@@ -13,23 +14,25 @@ R0 = 1
 SIGMA = 0
 EXT_CASE = 0
 
+
 def shuffle(arr: np.ndarray) -> np.ndarray:
     return arr
 
+
 def main():
-    R = np.ones((N))*R0
+    R = np.ones((N)) * R0
     R = shuffle(R)
     isFine = False
     N_subcl_perc = 0.1
     iter = 1
     while not isFine:
-        CCA_ok, PCA_ok = CCA_subcluster(R,N,DF,Kf, iter, N_subcl_perc, EXT_CASE)
+        CCA_ok, PCA_ok = CCA_subcluster(R, N, DF, Kf, iter, N_subcl_perc, EXT_CASE)
         isFine = CCA_ok and PCA_ok
         if not isFine:
             print("Restarting, wasnt able to generate aggregate")
 
-    
     print("Successfully generated aggregate")
+
 
 if __name__ == "__main__":
     main()
