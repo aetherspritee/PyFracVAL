737fa674996c68fc9e6d9f1a006155f038fbbeba
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-04-27 18:59:12 +0200

    Add test and docs group

--- flake.nix
+++ flake.nix
@@ -158,7 +158,8 @@
                   // {
                     pyfracval = [
                       # "explore"
-                      # "test"
+                      "test"
+                      "docs"
                     ];
                   }
                 );
