74afedd44ced843eafb33e7d02d1dd495aff022a
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-04-30 00:25:30 +0200

    feat: add semanting versioning

--- pyfracval/__init__.py
+++ pyfracval/__init__.py
@@ -7,18 +7,12 @@ __version__ = ""
 _authors = ""
 
 package_src = Path(__file__).parent
-# _version = package_src / "VERSION"
 _pyproject = package_src.parent / "pyproject.toml"
 if _pyproject.is_file():
     with open(_pyproject, "rb") as f:
         data = tomllib.load(f)
 
-        if "version" in data["project"]:
-            __version__ = data["project"]["version"]
-        else:
-            raise ValueError("Version not found in pyproject.toml")
-
         if "authors" in data["project"]:
             _authors = ",".join([x["name"] for x in data["project"]["authors"]])
         else:
-            raise ValueError("Version not found in pyproject.toml")
+            raise ValueError("Author not found in pyproject.toml")
