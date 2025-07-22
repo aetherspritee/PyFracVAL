2dee927f72ca22dd5bf8fa8a1d90f8ce880d5a0c
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-07-22 23:17:45 +0200

    misc: add batch file to test for difference configurations

---
+++ batch.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+rg=1000.0
+gstds=(1.2 1.5)
+
+kf=1.50
+
+ns=(
+	32
+	64
+	96
+	128
+	160
+	192
+	224
+	256
+)
+dfs=(1.4 1.6 1.8 2.0 2.2)
+
+for gstd in "${gstds[@]}"; do
+	for n in "${ns[@]}"; do
+		for df in "${dfs[@]}"; do
+			# echo "Generating $n $df $gstd"
+			echo "pyfracval -n \"$n\" --df \"$df\" --kf \"$kf\" --rp-g \"$rg\" --rp-gstd \"$gstd\""
+			pyfracval -n "$n" --df "$df" --kf "$kf" --rp-g "$rg" --rp-gstd "$gstd"
+		done
+	done
+done
+
+# pyfracval -n 8 --df 1.4 --kf 1.25 --rp-std 5
+# pyfracval -n 8 --df 1.6 --kf 1.25 --rp-std 5
+# pyfracval -n 8 --df 1.8 --kf 1.25 --rp-std 5
+# pyfracval -n 8 --df 2.0 --kf 1.25 --rp-std 5
+# pyfracval -n 8 --df 2.2 --kf 1.25 --rp-std 5
+
+# pyfracval -n 20 --df 1.4 --kf 1.25 --rp-std 5
+# pyfracval -n 20 --df 1.6 --kf 1.25 --rp-std 5
+# pyfracval -n 20 --df 1.8 --kf 1.25 --rp-std 5
+# pyfracval -n 20 --df 2.0 --kf 1.25 --rp-std 5
+# pyfracval -n 20 --df 2.2 --kf 1.25 --rp-std 5
+
+# pyfracval -n 48 --df 1.4 --kf 1.25 --rp-std 5
+# pyfracval -n 48 --df 1.6 --kf 1.25 --rp-std 5
+# pyfracval -n 48 --df 1.8 --kf 1.25 --rp-std 5
+# pyfracval -n 48 --df 2.0 --kf 1.25 --rp-std 5
+# pyfracval -n 48 --df 2.2 --kf 1.25 --rp-std 5
+
+# pyfracval -n 80 --df 1.4 --kf 1.25 --rp-std 5
+# pyfracval -n 80 --df 1.6 --kf 1.25 --rp-std 5
+# pyfracval -n 80 --df 1.8 --kf 1.25 --rp-std 5
+# pyfracval -n 80 --df 2.0 --kf 1.25 --rp-std 5
+# pyfracval -n 80 --df 2.2 --kf 1.25 --rp-std 5
